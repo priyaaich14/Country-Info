1771cfc9bf57c9b2be5403e7eac42f8d6bdc4e07
priyaaich14 <80387848+priyaaich14@users.noreply.github.com>
2024-11-26 23:41:57 +0530

    Final commit

---
+++ Frontend/country-info/src/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8',
+        secondary: '#9333ea',
+      },
+    },
+  },
+  plugins: [],
+};
+
